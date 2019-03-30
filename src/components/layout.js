e095e2643adeed60f1f810cd20fc365fc7e94358
Markos Konstantopoulos <markoskonstanto@hotmail.com>
2019-03-30 12:57:20 +0200

    Commented out the margin in layout.

--- src/components/layout.js
+++ src/components/layout.js
@@ -28,8 +28,8 @@ const Layout = ({ children }) => (
         <Header siteTitle={data.site.siteMetadata.title} />
         <div
           style={{
-            margin: `0 auto`,
-            maxWidth: 960,
+            // margin: `0 auto`,
+            // maxWidth: 960,
             padding: `0px 1.0875rem 1.45rem`,
             paddingTop: 0,
           }}
